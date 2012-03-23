a9e1c5ff47406a522444188cf1d055182699c7c8
sandroos <sandroos@e079296a-a135-4dcd-ac5b-8901e9a39efb>
2012-03-23 09:59:52 +0000

    Added copyright notice to licensing information.

--- vlsv/trunk/vlsv_common.h
+++ vlsv/trunk/vlsv_common.h
@@ -1,5 +1,7 @@
 /** This file is part of VLSV file format.
  * 
+ *  Copyright 2011, 2012 Finnish Meteorological Institute
+ * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
