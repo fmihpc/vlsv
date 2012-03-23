9d13e4d911743cd514c48d3313b316ce329cc4d6
sandroos <sandroos@users.noreply.github.com>
2012-03-23 09:59:52 +0000

    Added copyright notice to licensing information.

--- vlsvreader2.cpp
+++ vlsvreader2.cpp
@@ -1,4 +1,6 @@
 /** This file is part of VLSV file format.
+ * 
+ *  Copyright 2011, 2012 Finnish Meteorological Institute
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
