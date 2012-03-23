9a5bd85d20423dbddf724c465222dcc43a4e708d
sandroos <sandroos@e079296a-a135-4dcd-ac5b-8901e9a39efb>
2012-03-23 09:56:37 +0000

    Added LGPL licensing information to VLSV library.

--- vlsv/trunk/vlsvwriter2.cpp
+++ vlsv/trunk/vlsvwriter2.cpp
@@ -1,3 +1,19 @@
+/** This file is part of VLSV file format.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -275,7 +291,11 @@ bool VLSVWriter::open(const std::string& fname,MPI_Comm comm,const int& masterPr
 
 bool VLSVWriter::startMultiwrite(const string& datatype,const uint64_t& arraySize,const uint64_t& vectorSize,const uint64_t& dataSize) {
    // Clear per-thread storage:
-   multiwriteUnits[0].clear();
+     {
+	vector<list<VLSV::WriteUnit> > dummy(1);
+	multiwriteUnits.swap(dummy);
+     }
+   //multiwriteUnits[0].clear();
    multiwriteOffsets[0] = numeric_limits<unsigned int>::max();
 
    // Array datatype and byte size of each vector element are determined 
