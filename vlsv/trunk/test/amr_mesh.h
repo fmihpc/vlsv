5a65ff17a7d5f294cb762f372c0be7b306df7a20
sandroos <sandroos@e079296a-a135-4dcd-ac5b-8901e9a39efb>
2014-09-12 07:08:08 +0000

    Committing updates to VLSV adaptive mesh refinement test.

--- vlsv/trunk/test/amr_mesh.h
+++ vlsv/trunk/test/amr_mesh.h
@@ -30,22 +30,24 @@ class AmrMesh {
 	   const uint8_t& maxRefLevel);
    ~AmrMesh();
 
-   const std::unordered_map<uint64_t,uint8_t>::iterator begin();
-   const std::unordered_map<uint64_t,uint8_t>::iterator end();
-   
    bool initialize(const double& xmin,const double& xmax,const double& ymin,const double& ymax,
 		   const double& zmin,const double& zmax,const uint8_t refLevel=0);
    bool finalize();
-   
-   void getChildren(const uint64_t& globalID,std::vector<uint64_t>& children);
-   uint64_t getGlobalID(const uint32_t& refLevel,const uint32_t& i,const uint32_t& j,const uint32_t& k);
-   void getNeighbors(const uint64_t& globalID,std::vector<uint64_t>& neighborIDs);
-   void getIndices(const uint64_t& globalID,uint32_t& refLevel,uint32_t& i,uint32_t& j,uint32_t& k);
-   uint64_t getParent(const uint64_t& globalID);
-   void getSiblingNeighbors(const uint64_t& globalID,std::vector<uint64_t>& nbrs);
-   void getSiblings(const uint64_t& globalID,std::vector<uint64_t>& siblings);
-   size_t size() const;
 
+   const std::unordered_map<uint64_t,uint8_t>::iterator begin();
+   const std::unordered_map<uint64_t,uint8_t>::iterator end();
+   
+   void     getChildren(const uint64_t& globalID,std::vector<uint64_t>& children);
+   uint64_t getGlobalID(const uint32_t& refLevel,const uint32_t& i,const uint32_t& j,const uint32_t& k);
+   void     getNeighbors(const uint64_t& globalID,std::vector<uint64_t>& neighborIDs);
+   void     getIndices(const uint64_t& globalID,uint32_t& refLevel,uint32_t& i,uint32_t& j,uint32_t& k);
+   uint64_t getParent(const uint64_t& globalID);
+   void     getSiblingNeighbors(const uint64_t& globalID,std::vector<uint64_t>& nbrs);
+   void     getSiblings(const uint64_t& globalID,std::vector<uint64_t>& siblings);
+   size_t   size() const;
+
+   bool checkBlock(const uint64_t& globalID);
+   bool checkMesh();
    bool coarsen(const uint64_t& globalID);
    bool refine(const uint64_t& globalID);
 
@@ -54,14 +56,23 @@ class AmrMesh {
  private:
    AmrMesh();
 
-   uint32_t bbox[6];
-   std::unordered_map<uint64_t,uint8_t> globalIDs;
-   bool initialized;
-   std::vector<uint64_t> offsets;
-   uint64_t N_blocks0;
-   uint8_t refLevelMaxAllowed;
-   
-   double meshLimits[6];
+   uint32_t bbox[6];                                /**< Mesh bounding box. First three elements contain the 
+						     * number of blocks in x,y,z directions at the base grid level.
+						     * Last three indices contain the number of cells in each 
+						     * block in x,y,z directions.*/
+   double meshLimits[6];                            /**< Physical coordinates of the mesh boundaries. Elements contain
+						     * 0 = Minimum x-coordinate.
+						     * 1 = Maximum x-coordinate.
+						     * 2 = Minimum y-coordinate.
+						     * 3 = Maximum y-coordinate.
+						     * 4 = Minimum z-coordinate.
+						     * 5 = Maximum z-coordinate.*/
+   std::vector<uint64_t> offsets;                   /**< Block global ID offsets for each refinement level.*/
+   uint64_t N_blocks0;                              /**< Number of blocks at base grid level, equal to bbox[0]*bbox[1]*bbox[2].*/
+   uint8_t refLevelMaxAllowed;                      /**< Maximum allowed refinement level.*/
+
+   std::unordered_map<uint64_t,uint8_t> globalIDs;  /**< Hash table containing all existing blocks (global ID,refinement level).*/
+   bool initialized;                                /**< If true, mesh was initialized successfully.*/
 };
 
 #endif
