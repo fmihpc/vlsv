0f2c7ebf176202804f44b0e0ab7b58c08e0ac7fc
Ilja <2098356+iljah@users.noreply.github.com>
2026-06-23 11:26:10 +0000

    Remove hidden open warning from vlsv_reader_parallel.h

--- vlsv_reader_parallel.h
+++ vlsv_reader_parallel.h
@@ -42,6 +42,7 @@ namespace vlsv {
       uint64_t getBytesRead();
       double getReadTime() const;
       bool getUniqueAttributeValues(const std::string& tagName,const std::string& attribName,std::set<std::string>& output) const;
+      using Reader::open;
       bool open(const std::string& fname,MPI_Comm comm,const int& masterRank,MPI_Info mpiInfo=MPI_INFO_NULL);
       bool readArrayMaster(const std::string& tagName,const std::list<std::pair<std::string,std::string> >& attribs,
                            const uint64_t& begin,const uint64_t& amount,char* buffer);
